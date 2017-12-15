70d37cdc9900f6fe0d65e1015e581c4c83de1009
Gaston Mollica <mollica.gaston@gmail.com>
2017-12-15 10:39:40 +1000

    randomized function added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,18 @@ import Profile from './components/Profile';
 class App extends Component {
     state = {
       viewed: 0,
-      maxViews: 10
+      maxViews: 10,
+      gamble: true,
+      user: null
     }
+
+      randomGamble = () => {
+    const ranNum = Math.floor(Math.random() * 16) + 4;
+    this.setState({
+      gamble: false, 
+      maxViews: ranNum
+    })
+  }
 
     incrementViewed = () => {
      this.setState(prevState => ({
@@ -15,14 +25,40 @@ class App extends Component {
      }))
     }
 
+  getNextUser = () => {
+    this.setState({
+      user: null
+    })
+    fetch('https://randomuser.me/api/')
+    .then((response) => {
+      return response.json()
+    }).then(users => {
+      console.log(users.results[0].name.first)
+      this.setState({
+        user: users.results[0]
+      })
+      this.incrementViewed()
+    })
+  }
+
     render() {
-      const { viewed, maxViews } = this.state
+      const { viewed, maxViews, user, gamble } = this.state
     return (
       <div className="App">
         <p>You can view {viewed}/{maxViews} profiles</p>
-        <Profile viewed={viewed} maxViews={maxViews} incrementViewed={this.incrementViewed} />
+        <Profile
+         viewed={viewed} 
+         maxViews={maxViews} 
+         user={user} 
+         gamble={gamble}
+         randomGamble = {this.randomGamble}
+         getNextUser={this.getNextUser}
+         />
       </div>
     );
+  }
+    componentDidMount() {
+    this.getNextUser()
   }
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,48 +1,22 @@
 import React, { Component } from 'react';
 import { Button, Image, Progress } from 'reactbulma'
 
-
 class Profile extends Component {
 	state = {
-		gamble: true,
-		user: null
-	}
-	getNextUser = () => {
 
-		this.setState({
-			user: null
-		})
-
-		fetch('https://randomuser.me/api/')
-		.then((response) => {
-			return response.json()
-		}).then(users => {
-			console.log(users.results[0].name.first)
-			this.setState({
-				user: users.results[0]
-			})
-			this.props.incrementViewed()
-		})
-		
-	}
-
-	randomGamble = () => {
-		this.setState({
-			gamble: false
-		})
 	}
 
     render() {
-     const { user } = this.state
+    	const { user, gamble, getNextUser, randomGamble, maxViews, viewed } =this.props 
   	  return (
 	  	 <div>
 	  		{!user ? (<p>Loading...</p>) : (
 	  			<div>
-	  			<Progress info value={this.props.viewed} max={this.props.maxViews}>{this.props.viewed}%</Progress>
+	  			<Progress info value={viewed} max={maxViews}>{viewed}%</Progress>
 	  			<Image is="128x128" src={user.picture.medium} />
 	  			<p>{ user.name.first }</p>	
-	  			{this.props.viewed < this.props.maxViews && <Button info onClick={this.getNextUser}>Next</Button> }
-	  			{ this.state.gamble && <Button onClick={this.randomGamble()}>Gamble</Button> }
+	  			{viewed < maxViews && <Button info onClick={getNextUser}>Next</Button> }
+	  			{ gamble && <Button onClick={randomGamble}>Gamble</Button> }
 	  	    </div>
 	  		)}
 	  	</div>
@@ -50,9 +24,6 @@ class Profile extends Component {
   }
 
 
-	componentDidMount() {
-		this.getNextUser()
-	}
 
 }
 export default Profile;
